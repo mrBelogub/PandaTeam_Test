341b6fc29436f9d6914740c44b79777a0909d27b
Сергій Білогуб <67222004+mrBelogub@users.noreply.github.com>
2024-02-02 16:18:32 +0200

    Створив точку входу для апі

---
+++ api/index.php
@@ -0,0 +1,49 @@
+<?php
+
+// Розпочинаємо сессію
+session_start();
+
+// Отримуємо що там хотів від нас користувач
+$action = $_GET['action'] ?? null;
+var_dump($action);
+
+// Якщо action прийшов пустим - видаємо помилку
+if(empty($action)) {
+    echo 'Unknown action';
+    http_response_code(400);
+    exit;
+}
+
+// Якщо користувач хотів не авторизуватись або вийти - перевіряємо, чи авторизованний він
+if ($action != "login" && $action != "logout") {
+    if (!isset($_SESSION['user'])) {
+        echo "401 Unauthorized";
+        http_response_code(401);
+        exit;
+    }
+}
+
+// Формуємо шлях до файлу обробника
+$file_path = "src/" . $action . ".php";
+
+// Якщо в нас нема такого обробника - видаємо помилку
+if (!file_exists($file_path)) {
+    echo 'Action not found';
+    http_response_code(404);
+    exit;
+}
+
+// Підключаємося до файлу залежностей
+require_once "src/Core/requires.php";
+
+try {
+    // Намагаємося виконати дію
+    require_once $file_path;
+} catch (Exception $e) {
+    // Якщо чомусь не виходить - видаємо помилку
+    $errorMessage = $e->getMessage();
+
+    echo "Error message: $errorMessage";
+    http_response_code(400);
+    exit;
+}
