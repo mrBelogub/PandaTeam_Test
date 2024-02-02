0724cb873e0a9470022ccde924c9314ea848427e
Сергій Білогуб <67222004+mrBelogub@users.noreply.github.com>
2024-02-02 20:50:33 +0200

    Зробив роботу з об'явою

---
+++ api/src/Controllers/Advertisement.php
@@ -0,0 +1,41 @@
+<?php
+
+class Advertisement{
+
+    private string $url;
+    public string $slug;
+    public int $id;
+
+    public function __construct(string $url)
+    {
+        $this->url = $url;
+        $this->slug = $this->getSlug();
+        $this->id = $this->getId();
+    }
+
+    private function getSlug(){
+        $path = parse_url($this->url, PHP_URL_PATH);
+        $basename = basename($path);
+        if(empty($basename)){
+            throw new Exception("При обробці посилання виникла помилка: не вдалось знайти сторінку");
+        }
+        return $basename;
+    }
+
+    private function getId(){
+        $data = DB::getOne("SELECT `id` FROM `advertisements` WHERE `slug` = :slug", ["slug" => $this->slug]);
+        $id = $data["id"] ?? null;
+
+        if(empty($id)){
+            $id = $this->create();
+        }
+
+        return $id;
+
+    }
+
+    private function create(){
+        $id = DB::insert("INSERT INTO `advertisements` (`slug`) VALUES (:slug)", ["slug" => $this->slug]);
+        return $id;
+    }
+}
--- api/src/Core/validator.php
+++ api/src/Core/validator.php
@@ -9,11 +9,23 @@ class Validator
      * @param mixed $var Змінна
      * @throws Exception Помилка у разі якщо змінна пуста
      */
-    public static function isEmpty(string $name, $var)
+    public static function isEmpty(string $name, mixed $var)
     {
         $trimmed_var = trim($var);
         if(empty($trimmed_var)) {
             throw new Exception($name . " is empty!");
+        }
+    }
+
+    public static function isEmailCorrect(string $email){
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            throw new Exception("Email вказано не вірно!");
+        }
+    }
+
+    public static function isURLCorrect(string $url){
+        if (!filter_var($url, FILTER_VALIDATE_URL)) {
+            throw new Exception("Посилання вказано не вірно!");
         }
     }
 }
