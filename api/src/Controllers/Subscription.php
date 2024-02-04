b9caf65504a925aa52173b68e8d34ca134726840
Сергій Білогуб <67222004+mrBelogub@users.noreply.github.com>
2024-02-04 19:35:39 +0200

    Виніс обробку листів до юзерів з метода

--- api/src/Controllers/Subscription.php
+++ api/src/Controllers/Subscription.php
@@ -37,19 +37,20 @@ class Subscription
                                                 LEFT JOIN `advertisements` ON `advertisements`.`id` = `subscriptions`.`advertisement_id`
                                                 WHERE `subscriptions`.`user_id` = :user_id
                                                 ORDER BY `subscriptions`.`id` DESC;", ["user_id" => $user_id]);
-                                                // NOTE: Можна було б зробити інші варіанти сортування
-                                                // Наприклад: останні додані спочатку, або остання змінена ціна спочатку
-                                                // Або додати варіацію сортування в залежності від вибору на фронті
-                                                // А оскільки вимог не було - я вирішив зробити так, щоб нові підписки були зверху
-                                                // Але якби це було б не тестове - можна було б обсудити як треба зробити
+        // NOTE: Можна було б зробити інші варіанти сортування
+        // Наприклад: останні додані спочатку, або остання змінена ціна спочатку
+        // Або додати варіацію сортування в залежності від вибору на фронті
+        // А оскільки вимог не було - я вирішив зробити так, щоб нові підписки були зверху
+        // Але якби це було б не тестове - можна було б обсудити як треба зробити
         return $subscription_data;
     }
 
 
     public static function notificateUsersAboutPriceChange(array $changed_prices)
     {
-
         $advertisement_ids = array_column($changed_prices, "advertisement_id");
+
+        $formatted_prices_array = self::getFormattedPriceArray($changed_prices);
 
         $matched_users = DB::execRequest("SELECT `users`.`email`, GROUP_CONCAT(`advertisements`.`id`) AS advertisement_ids
                                             FROM `users`
@@ -58,8 +59,22 @@ class Subscription
                                             WHERE `advertisements`.`id` IN (:ids)
                                             GROUP BY `users`.`id`", ['ids' => $advertisement_ids]);
 
+        $users_with_price_changes_messages = self::getUsersWithPriceChangesMessages($matched_users, $formatted_prices_array);
 
+        $mail_subject = MAIL::TEMPLATE_PRICE_CHANGE_SUBJECT;
+        foreach ($users_with_price_changes_messages as $current_data) {
 
+            $user_email = $current_data["email"];
+
+            $message_part = $current_data["message_part"];
+            $message = MAIL::TEMPLATE_PRICE_CHANGE_MAIL . $message_part;
+
+            Mail::send($user_email, $mail_subject, $message);
+        }
+    }
+
+    private static function getFormattedPriceArray(array $changed_prices): array
+    {
         $formatted_prices_array = array_reduce($changed_prices, function ($result, $price) {
             $floated_old_price = Price::formatToFloat($price["old_price"]);
             $floated_new_price = Price::formatToFloat($price["new_price"]);
@@ -72,6 +87,11 @@ class Subscription
             return $result;
         }, []);
 
+        return $formatted_prices_array;
+    }
+
+    private static function getUsersWithPriceChangesMessages(array $matched_users, array $formatted_prices_array): array
+    {
         $users_with_price_changes_messages = array_map(function ($user) use ($formatted_prices_array) {
             $advertisement_ids = explode(',', $user['advertisement_ids']);
 
@@ -83,16 +103,6 @@ class Subscription
             return $user;
         }, $matched_users);
 
-
-        $mail_subject = MAIL::TEMPLATE_PRICE_CHANGE_SUBJECT;
-        foreach ($users_with_price_changes_messages as $current_data) {
-
-            $user_email = $current_data["email"];
-
-            $message_part = $current_data["message_part"];
-            $message = MAIL::TEMPLATE_PRICE_CHANGE_MAIL . $message_part;
-
-            Mail::send($user_email, $mail_subject, $message);
-        }
+        return $users_with_price_changes_messages;
     }
 }
