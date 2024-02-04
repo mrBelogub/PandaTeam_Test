c332b3745ab1cc731d6223b8e8fdce5bc739152a
Сергій Білогуб <67222004+mrBelogub@users.noreply.github.com>
2024-02-04 19:59:31 +0200

    Переніс формування статус коду до ехо

--- api/index.php
+++ api/index.php
@@ -8,18 +8,18 @@ $action = $_GET['action'] ?? null;
 
 // Якщо action прийшов пустим - видаємо помилку
 if(empty($action)) {
-    echo 'Unknown action';
     http_response_code(400);
-    exit;
+    echo 'Unknown action';
+    exit();
 }
 
 const ACTION_WITHOUT_AUTH = ["signUp", "signIn", "subscribeWithoutAuth", "activateProfile"];
 
 if (!in_array($action, ACTION_WITHOUT_AUTH)){
     if (!isset($_SESSION['user_id'])) {
-        echo "401 Unauthorized";
         http_response_code(401);
-        exit;
+        echo "401 Unauthorized";
+        exit();
     }
 }
 
@@ -28,9 +28,9 @@ $file_path = "src/Requests/" . $action . ".php";
 
 // Якщо в нас нема такого обробника - видаємо помилку
 if (!file_exists($file_path)) {
-    echo 'Action not found';
     http_response_code(404);
-    exit;
+    echo 'Action not found';
+    exit();
 }
 
 // Підключаємося до файлу залежностей
@@ -42,8 +42,8 @@ try {
 } catch (Exception $e) {
     // Якщо чомусь не виходить - видаємо помилку
     $errorMessage = $e->getMessage();
-
-    echo "Error message: $errorMessage";
     http_response_code(400);
-    exit;
+    echo "Error message: $errorMessage";
+    
+    exit();
 }
