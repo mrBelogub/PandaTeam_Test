379c3a532f980b5e3d9e5ee0dee6b7ce8743011b
Сергій Білогуб <67222004+mrBelogub@users.noreply.github.com>
2024-02-05 15:26:26 +0200

    Фікс відправки пошти

--- api/src/Core/mail.php
+++ api/src/Core/mail.php
@@ -8,17 +8,41 @@ class Mail{
     public const TEMPLATE_PRICE_CHANGE_SUBJECT = "На деякі товари у ваших підписках змінилась ціна!";
     public const TEMPLATE_PRICE_CHANGE_MAIL = "Ось, подивiться!<br><br>";
 
-    private const FROM = "PandaTeam@belogub.com";
-
     public static function send(string $email, string $subject, string $message){
 
         $message = "<html><body>".$message."</body></html>";
-        $headers = "MIME-Version: 1.0" . "\r\n";
-        $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-        $headers .= "From: " . self::FROM . "\r\n";
         
-        $additional_params = "-f".self::FROM;
-        
-        return mail($email, $subject, $message, $headers, $additional_params);
+        return self::sendByBelogubCom($email, $subject, $message);
+    }
+
+    private static function sendByBelogubCom($email, $subject, $message){
+
+        // NOTE: ну звичайно ж так робити не можна!
+        // По хорошому треба використовувати або дефолтний mail() (який не працює на локалхості без танців с бубнами),
+        // або що ще краще - умовний PHPMailer, тобто налаштувати SMTP і через реквізити підключитись.
+        // Але оскільки була вимога "не можна використовувати бібліотекі та фреймворки",
+        // а мені важливо було щоб тестувальники змогли перевірити навіть запустивши докер на локальній машині
+        // (та щоб інструкція не займала більше місця аніж усі файли проекту) - я пішов на таку хитрість.
+
+        $url = 'https://email.belogub.com/';
+
+        $data = [
+            "email" => $email,
+            "subject" => $subject,
+            "message" => $message
+        ];
+
+        $ch = curl_init($url);
+
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        $response = curl_exec($ch);
+
+        curl_close($ch);
+
+        $bool_response = json_decode($response);
+        return $bool_response;
     }
 }
